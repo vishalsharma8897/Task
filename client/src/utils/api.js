8a8915cc2d0bf403963bfded86c2c9fec2541717
vishalsharma8897 <vishalsharma19052001@gmail.com>
2024-01-01 18:38:27 +0530

    changed host

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-export const host =  "http://localhost:8080" ;
+export const host =  "https://goodspace-backend-65qq.onrender.com";
 
 
 export const registerRoute = `${host}/api/auth/register`                            
